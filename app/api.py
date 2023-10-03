0a0ec48c1a3685974c5a52f627c623ce5c61948f
katie-gardner <katharine.gardner@and.digital>
2023-10-03 15:32:54 +0100

    Prompt engineering

--- app/api.py
+++ app/api.py
@@ -17,6 +17,9 @@ FINE_TUNE_ID = os.getenv("FINE_TUNE_ID")
 logger = logging.getLogger("Hackathon")
 openai.api_key = OPENAI_API_KEY
 
+PROMPT_ENGINEERING_101 = """
+Use the input data to predict yearly expenditure. Please output your answer without any extra information in the format: food: a; clothing: b; entertainment: c; transport: d
+"""
 
 def predict(*, year: int, inflation: float, income: float) -> dict:
     """
@@ -24,7 +27,7 @@ def predict(*, year: int, inflation: float, income: float) -> dict:
     returns the categories and expenditure as a dictionary, or an error if the response cannot be parsed.
     """
     prompt = generate_prompt(year=year, inflation=inflation, income=income)
-    temporary_extra = "\nUse the input data to predict yearly expenditure. Please output your answer without any extra information in the format: food: a; clothing: b; entertainment: c; transport: d"
+    temporary_extra = PROMPT_ENGINEERING_101
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -42,8 +45,8 @@ def predict(*, year: int, inflation: float, income: float) -> dict:
             result["success"] = True
             return result
         except ValueError:
-            logger.error(f"error: Unable to parse response, response={response}")
-
+            logger.error("error: Unable to parse response")
+            print(response)
     logger.info("API call unsuccessful")
 
     return {
