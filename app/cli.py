76938ae95dd430694dacced6ba9e02917af6f732
Mebin Abraham <mebin95+work@gmail.com>
2023-10-03 13:25:29 +0100

    .

---
+++ app/cli.py
@@ -0,0 +1,38 @@
+import argparse
+from _decimal import Decimal
+
+
+def calculate_future_income(income, inflation, year):
+    """Implement this"""
+    return 0
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Calculate future income based on given parameters."
+    )
+    parser.add_argument("income", type=Decimal, help="Current income")
+    parser.add_argument(
+        "inflation", type=Decimal, help="Inflation rate (as a percentage)"
+    )
+    parser.add_argument("year", type=int, help="Number of years in the future")
+    parser.add_argument(
+        "salary_increase",
+        type=Decimal,
+        help="Annual salary increase rate (as a percentage)",
+    )
+
+    args = parser.parse_args()
+
+    salary_increase = args.salary_increase or 1
+    # calculate income after salary increase
+    income = args.income * (1 + salary_increase / 100)
+
+    future_income = calculate_future_income(income, args.inflation, args.year)
+    print(
+        f"Projected income after {args.year} years: ${future_income:.2f} with {args.inflation}% inflation and {args.salary_increase}% salary increase"
+    )
+
+
+if __name__ == "__main__":
+    main()
