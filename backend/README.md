c6715aa2d3c373ff8e8829455bda47abe0dcd867
Mebin Abraham <mebin95+work@gmail.com>
2023-10-03 11:17:04 +0100

    Initial

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Basic Python Project Template
+
+A simple template to get started with a python project.
---
+++ backend/__init__.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+
+import coloredlogs
+
+# __all__ = ["SomeClassOrMethod"]
+
+
+log_level = logging.INFO
+
+logger = logging.getLogger("backend")
+logger.setLevel(log_level)
+format_string = "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+
+if "COLOREDLOGS_LEVEL_STYLES" not in os.environ:
+    coloredlogs.DEFAULT_LEVEL_STYLES = {
+        **coloredlogs.DEFAULT_LEVEL_STYLES,
+        "trace": {"color": 246},
+        "critical": {"background": "red"},
+        "debug": coloredlogs.DEFAULT_LEVEL_STYLES["info"],
+    }
+
+if "COLOREDLOGS_LOG_FORMAT" not in os.environ:
+    coloredlogs.DEFAULT_LOG_FORMAT = format_string
+
+if "COLOREDLOGS_LOG_LEVEL" not in os.environ:
+    coloredlogs.DEFAULT_LOG_LEVEL = log_level
+
+coloredlogs.install(logger=logger, stream=sys.stdout)
