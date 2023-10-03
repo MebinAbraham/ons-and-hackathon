aabbbf108a5a3263c43820517d402612a013c385
Mebin Abraham <mebin95+work@gmail.com>
2023-10-03 14:56:02 +0100

    Link up frontend to backend

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from random import random, randint
+
 import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
@@ -11,9 +13,39 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def predict(*, year: int, inflation: float, income: float) -> dict:
+    return {
+        "food": randint(0, 1000),
+        "clothing": randint(0, 1000),
+        "entertainment": randint(0, 1000),
+        "transport": randint(0, 1000),
+    }
+
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request, "data": {}})
+    output_data = {"request": request}
+    return templates.TemplateResponse("base.html", output_data)
+
+
+@app.get("/predict")
+async def _predict(request: Request, year: int, inflation: float, income: float) -> dict:
+    year = year or 2000
+    inflation = inflation or 0
+    income = income or 0
+
+    data = predict(
+        year=year,
+        inflation=inflation,
+        income=income,
+    )
+
+    total = sum(data.values())
+    data["total"] = total
+    data["year"] = year
+    data["inflation"] = inflation
+    data["income"] = income
+    return data
 
 
 if __name__ == "__main__":
